c112f10aa3babda8ff09ab5afe9f5117d47d9f5e
alexxtz <alexandr_nikolaenko@ukr.net>
2016-05-19 17:36:19 +0300

    Update lab4_Boroda.pl

--- lab4_Boroda.pl
+++ lab4_Boroda.pl
@@ -5,24 +5,26 @@ my $fileName;
 my $count;
 if (defined $ARGV[0]) {
     $fileName = $ARGV[0];
-    if (defined $ARGV[1]) {
-        $count = $ARGV[1];
-        open (my $fh, "<", "$fileName") or die "Can't open the file.\n";
-        while (<$fh>) {
-            chomp;
-            next unless ($_);
-            system "ping -c $count $_";
-            
-
-        }
-    } else {
-        print "You don't enter count.\n";
-    }
-
-
-
-
 } else {
-    print "You don't enter fileName.\n";
+    die "You don't enter fileName.\n";
 }
+    
+if (defined $ARGV[1]) {
+    $count = $ARGV[1];
+} else {
+    die "You don't enter count.\n";
+}
+
+open (my $fh, "<", "$fileName") or die "Can't open the file.\n";
+while (<$fh>) {
+    chomp;
+    next unless ($_);
+    system "ping -c $count $_"; # тут наверное нужно куда-то присваивать результат
+}
+
+
+
+
+
+
 #neskolko v stochke
